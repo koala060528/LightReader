d5333c73aa94481a68cd922b12fe4e8678ca699e
SRT <srt060528@hotmail.com>
2018-06-07 15:15:39 +0800

    增加搜索书籍的API

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, db
 from app.models import User, Subscribe
 import json
 from flask import render_template, flash, redirect, url_for, request, jsonify
-from app.forms import LoginForm, RegistrationForm,ResetPasswordForm,ResetPasswordRequestForm
+from app.forms import LoginForm, RegistrationForm, ResetPasswordForm, ResetPasswordRequestForm
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
 from datetime import datetime
@@ -19,19 +19,19 @@ def get_response(url):
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
-        current_user.last_seen=datetime.utcnow()
+        current_user.last_seen = datetime.utcnow()
         # 教程上说不需要加这一行，亲测需要
         db.session.add(current_user)
         db.session.commit()
 
 
-@app.route('/login',methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        u = User.query.filter_by(name = form.username.data).first()
+        u = User.query.filter_by(name=form.username.data).first()
         if u is None or not u.check_password(form.password.data):
             flash('登录失败')
             return redirect('login')
@@ -41,7 +41,7 @@ def login():
         if not next_page or url_parse(next_page).decode_netloc() != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html',title = '登录', form = form)
+    return render_template('login.html', title='登录', form=form)
 
 
 @app.route('/logout')
@@ -56,31 +56,31 @@ def register():
     pass
 
 
-@app.route('/reset_password_request',methods=['POST','GET'])
+@app.route('/reset_password_request', methods=['POST', 'GET'])
 def reset_password_request():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = ResetPasswordRequestForm()
     # 少了括号会报错validate_on_submit() missing 1 required positional argument: 'self'
     if form.validate_on_submit():
-        user=User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user:
             # send_password_reset_email(user)
             pass
             # todo
         flash('Check your email for the instructions to reset your password')
         return redirect(url_for('login'))
-    return render_template('reset_password_request.html',title='Reset Password',form=form)
+    return render_template('reset_password_request.html', title='Reset Password', form=form)
 
 
-@app.route('/reset_password/<token>',methods=['GET', 'POST'])
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    user=User.verify_reset_password_token(token)
+    user = User.verify_reset_password_token(token)
     if not user:
         return redirect(url_for('index'))
-    form=ResetPasswordForm()
+    form = ResetPasswordForm()
     if form.validate_on_submit():
         user.set_password(form.password.data)
         db.session.commit()
@@ -98,7 +98,7 @@ def index():
     data['subscribe'] = []
     for s in current_user.subscribing:
         js = get_response('http://api.zhuishushenqi.com/book/' + s.book_id)
-        data['subscribe'] . append({
+        data['subscribe'].append({
             'title': js['title'],
             '_id': s.book_id,
             'last_chapter': js['lastChapter'],
@@ -145,10 +145,8 @@ def get_chapter_detail():
     return jsonify(data)
 
 
-# app.route()
-
-
-
-
-
-
+@app.route('/api/v1/search/', methods=['GET', 'POST'])
+def search():
+    query = request.args.get('query')
+    data = get_response('http://api.zhuishushenqi.com/book/fuzzy-search/?query=' + query)
+    return render_template('search.html', data=data, title='搜索结果：%s' % query)
