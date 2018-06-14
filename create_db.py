47136ad3c9c5c86204e15e5047339d14f83a5cc8
SRT <srt060528@hotmail.com>
2018-06-14 18:57:59 +0800

    调整字体大小

--- create_db.py
+++ create_db.py
@@ -2,10 +2,10 @@ import pymysql
 from config import Config
 from app import db
 
-conn=pymysql.connect(host=Config.D_HOST,port=Config.D_PORT,user=Config.D_USER,passwd=Config.D_PASSWORD)
-cursor=conn.cursor()
+conn = pymysql.connect(host=Config.D_HOST, port=Config.D_PORT, user=Config.D_USER, passwd=Config.D_PASSWORD)
+cursor = conn.cursor()
 cursor.execute("show databases like 'novelreader'")
-create_db=cursor.fetchall()
+create_db = cursor.fetchall()
 if not create_db:
     cursor.execute('create database novelreader')
 cursor.close()
